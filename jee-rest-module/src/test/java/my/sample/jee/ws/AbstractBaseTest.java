84e5ba61fb9f45af19fbf666b1f8d7df64a060b5
rzo1 <richard@zowalla.com>
2018-09-12 13:52:55 +0200

    working solution

--- jee-rest-module/src/test/java/my/sample/jee/ws/AbstractBaseTest.java
+++ jee-rest-module/src/test/java/my/sample/jee/ws/AbstractBaseTest.java
@@ -1,17 +1,24 @@
 package my.sample.jee.ws;
 
 import my.sample.jee.ws.api.ws.DemoWSEndpoint;
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.johnzon.jaxrs.jsonb.jaxrs.JsonbJaxrsProvider;
+import org.apache.johnzon.mapper.access.FieldAccessMode;
 import org.apache.openejb.OpenEjbContainer;
 import org.apache.openejb.junit.jee.EJBContainerRule;
 import org.apache.openejb.junit.jee.InjectRule;
 import org.apache.openejb.junit.jee.config.Properties;
 import org.apache.openejb.junit.jee.config.Property;
 import org.apache.openejb.junit.jee.resources.TestResource;
+import org.apache.openejb.server.cxf.rs.johnzon.TomEEJsonbProvider;
 import org.junit.ClassRule;
 import org.junit.Rule;
 
 import javax.naming.Context;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author rz
@@ -42,9 +49,20 @@ public class AbstractBaseTest {
     @TestResource
     private Context ctx;
 
-    protected DemoWSEndpoint createRestServiceClient() {
-        return JAXRSClientFactory.create("http://localhost:" + PORT + CONTEXT_PATH + DemoWSEndpoint.API_PATH,
-                DemoWSEndpoint.class);
+    protected WebClient createWebClient(String method) {
+        TomEEJsonbProvider provider = new TomEEJsonbProvider();
+        provider.setAccessMode(new FieldAccessMode(true,true));
+        provider.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        provider.setLocale(Locale.GERMANY);
+
+        List<JsonbJaxrsProvider> providers = new ArrayList<>();
+        providers.add(provider);
+
+        WebClient httpClient = WebClient.create("http://localhost:" + PORT + CONTEXT_PATH, providers);
+        httpClient.path(DemoWSEndpoint.API_PATH + method);
+        httpClient.accept(MediaType.APPLICATION_JSON_TYPE);
+        httpClient.type(MediaType.APPLICATION_JSON_TYPE);
+        return httpClient;
     }
 
 }
--- jee-rest-module/src/test/java/my/sample/jee/ws/DemoWSEndpointTest.java
+++ jee-rest-module/src/test/java/my/sample/jee/ws/DemoWSEndpointTest.java
@@ -1,7 +1,7 @@
 package my.sample.jee.ws;
 
-import my.sample.jee.ws.api.ws.DemoWSEndpoint;
 import my.sample.jee.ws.xto.DemoResponse;
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.openejb.junit.jee.EJBContainerRunner;
 import org.apache.openejb.junit.jee.config.PropertyFile;
 import org.apache.openejb.testing.EnableServices;
@@ -10,8 +10,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 @EnableServices(value = "jaxrs")
 @PropertyFile("openejb-junit.properties")
@@ -19,8 +21,6 @@ import static org.junit.Assert.fail;
 public class DemoWSEndpointTest extends AbstractBaseTest {
 
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(DemoWSEndpointTest.class);
-    private DemoWSEndpoint endpoint = createRestServiceClient();
-
 
     @Before
     public void setup() {
@@ -30,8 +30,26 @@ public class DemoWSEndpointTest extends AbstractBaseTest {
     @Test
     public void testGetDemos() {
         try {
-            DemoResponse demos = endpoint.getDemo();
+            WebClient client = createWebClient("/demos");
+            DemoResponse demos = client.get(DemoResponse.class);
             assertNotNull(demos);
+        } catch (RuntimeException e) {
+            logger.error(e.getLocalizedMessage(), e);
+            fail(e.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void testPostDemo() {
+        try {
+            WebClient client = createWebClient("/demos");
+
+            Date myDate = new Date();
+            DemoResponse post = new DemoResponse("ich-bin", myDate);
+
+            DemoResponse demos = client.post(post, DemoResponse.class);
+            assertNotNull(demos);
+            assertEquals(myDate.toInstant().truncatedTo(ChronoUnit.SECONDS), demos.getTheDate().toInstant().truncatedTo(ChronoUnit.SECONDS));
         } catch (RuntimeException e) {
             logger.error(e.getLocalizedMessage(), e);
             fail(e.getLocalizedMessage());
